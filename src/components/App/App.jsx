663b334ea85eac5d7db71e17e3c570858494c2d8
Matt Oliva <Mtoliva915@gmail.com>
2021-03-30 23:01:10 -0400

    Route Edits

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { Route, Link } from 'react-router-dom';
 import HomePage from '../HomePage/HomePage';
 import ContactPage from '../ContactPage/ContactPage';
+import ServicePage from '../ServicePage/ServicePage';
+import AboutUsPage from '../AboutUsPage/AboutUsPage';
 
 import './App.scss';
 
@@ -12,10 +14,15 @@ function App() {
       <header className="header">
         <Link className="nav-btn" to="/">Home</Link>
         <Link className="nav-btn" to="/contact">Contact</Link>
+        <Link className="nav-btn" to="/services">Services</Link>
+        <Link className="nav-btn" to="/about">About Us</Link>
       </header>
 
       <Route exact path="/"><HomePage /></Route>
       <Route path="/contact"><ContactPage /></Route>
+      <Route path="/services"><ServicePage /></Route>
+      <Route path="/about"><AboutUsPage /></Route>
+
       <footer className="footer">Footer</footer>
     </div>
   );

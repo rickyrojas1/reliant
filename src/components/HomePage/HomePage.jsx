75a15893afb698ab158274c2003a08fa23af2e11
Matt Oliva <Mtoliva915@gmail.com>
2021-04-06 01:10:01 -0400

    Fonts and Colors

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -4,7 +4,13 @@ import './HomePage.scss';
 
 const HomePage = () => (
   <div className="content">
-    <div className="img" />
+    <div className="img">
+      <h1 className="home-page-header">
+        Reliant Fire Hose Testing Inc.
+      </h1>
+      <div />
+    </div>
+    {/* <div className="img" /> */}
     {/* <div className="selling-points">
       <SellingPoint title="Services 1" bodyText="lorem ipsum hello there this is text" />
       <SellingPoint title="Services 2" bodyText="lorem ipsum hello there this is text" />
